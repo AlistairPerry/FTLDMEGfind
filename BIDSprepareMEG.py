f6458813bf44f224f6ee521a6c18b9cd801aa0b2
Alistair Perry <alistairperry123@gmail.com>
2022-10-25 17:08:48 +0100

    start

---
+++ BIDSprepareMEG.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 16:33:50 2022
+
+@author: alistairperry
+"""
+
+
+#newdf
+
+
+#bids id
+
+sub-%4d
+
+
+#bids raw dir
+
+etc
+
+
+#bids raw file name
+
+add
+
+
+#find file
+
+#lower case all file names
+
+see if file exits
+
+
+if not:
+    
+    rsc1
+    
+    closed
+    
+
+if missing
+
+add1
+
+if exits:
+    
+parallelize     
+
+#mne-python scan length
+
+
+
