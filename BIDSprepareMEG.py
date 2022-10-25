fcf8f40c0c6400dd252f14ea8d2c17ace9fe5b6a
Alistair Perry <alistairperry@Alistairs-NewMacBook-Pro.local>
2022-10-25 17:35:18 +0100

    add BIDs ID

--- BIDSprepareMEG.py
+++ BIDSprepareMEG.py
@@ -6,13 +6,36 @@ Created on Tue Oct 25 16:33:50 2022
 @author: alistairperry
 """
 
+'''
+Setup
 
-#newdf
+'''
 
 
-#bids id
+import pandas as pd
 
-sub-%4d
+
+
+input_file = '/Users/alistairperry/Documents/Cambridge/Project_3/AllMEGlist_Compiled.csv'
+
+
+MEGdata = pd.read_csv(input_file)
+
+
+MEGdata_wBIDs = MEGdata.copy()
+
+
+BIDs_ID = []
+
+for x in range(1, len(MEGdata_wBIDs['S_ID'])+1):
+    
+    BIDs_ID.append("sub-Sub{:04d}".format(x)) 
+    
+
+MEGdata_wBIDs['BIDs_ID'] = BIDs_ID
+
+
+
 
 
 #bids raw dir
