41f0949499439b46dfa2045d6ae02065e50b9edf
AlistairPerry <alistairperry123@gmail.com>
2022-11-03 13:47:39 +0000

    complete MRI transfer

--- Setup/A_Scripts/integrateMRIdata.m
+++ Setup/A_Scripts/integrateMRIdata.m
@@ -1,4 +1,4 @@
-function integrateMRIdata(BIDS_BASEDIR)
+function integrateMRIdata(BASE_BIDS_DIR)
 
 
 MEGsubjlist = '/imaging/rowe/users/ap09/Projects/FTD-MEG-MEM_3/Misc/MEGsubjlist_wBIDS.csv';
@@ -7,16 +7,16 @@ MEGsubjlist = '/imaging/rowe/users/ap09/Projects/FTD-MEG-MEM_3/Misc/MEGsubjlist_
 %Ensure MRI_ID is read as string
 opts = detectImportOptions(MEGsubjlist);
 opts = setvartype(opts, 'MRI_ID', 'string');  %or 'char' if you prefer
+
 MEGsubjlist_tab = readtable(MEGsubjlist, opts);
 
 
-new_bids_fname_pfix = '_ses-meg1_T1w.nii.gz';
 
-%MEGsubjlist_tab.found_MRI =
+new_bids_fname_pfix = '_ses-meg1_T1w.nii';
 
-%MEGsubjlist_tab.BIDS_fname_MRI =
 
-%MEGsubjlist_tab.MRI_scanner =
+
+anat_deriv_DIR = [BASE_BIDS_DIR '' 'derivatives' '/' 'meg_derivatives'];
 
 
 %% Setup dir paths
@@ -32,9 +32,14 @@ dir_path4 = '/imaging/rowe/users/lh01/FTD2010/MRIscans/PSP_Controls';
 dir_path5 = '/imaging/rowe/users/lh01/FTD2010/MRIscans';
 
 
+
 for subj = 1:length(MEGsubjlist_tab.BIDS_ID)
     
     MRI_path = [];
+    
+    new_BIDSDIR = [BASE_BIDS_DIR '' MEGsubjlist_tab.BIDS_ID{subj} '/' 'anat/'];
+        
+    new_BIDSDIR_deriv = [anat_deriv_DIR '/' MEGsubjlist_tab.BIDS_ID{subj} '/' 'ses-meg1' '/' 'anat/'];
     
     
     %For now only include those with dir
@@ -42,28 +47,34 @@ for subj = 1:length(MEGsubjlist_tab.BIDS_ID)
         
         continue
         
+        
     else
         
+        try eval(sprintf('!mkdir -p %s', new_BIDSDIR)); end
         
-        %need to find files according to rules
+        try eval(sprintf('!mkdir -p %s', new_BIDSDIR_deriv)); end
+        
+        
+        %Need to find files according to dir rules
         
         if strcmp(MEGsubjlist_tab.MRI_Dir(subj), dir_path1)
             
-            MRI_path = [dir_path1 '/' MEGsubjlist_tab.MRI_ID(subj) '_structural.nii'];
+            MRI_path = strjoin([dir_path1 '/' MEGsubjlist_tab.MRI_ID(subj) '_structural.nii'], '');
             
             
         elseif strcmp(MEGsubjlist_tab.MRI_Dir(subj), dir_path2)
             
             subjfolder = strjoin([MEGsubjlist_tab.MRI_Dir(subj) '/' MEGsubjlist_tab.MRI_ID(subj)], '');
             
+            
             if isfolder(subjfolder)
                 
-                S = dir(subjfolder, '*.nii');
+                S = dir(fullfile(subjfolder, '*.nii'));
                 
                 S(startsWith({S.name},'y_')) = [];
                 
                 
-                MRI_path = fullfile(subjfolder, S.name);
+                MRI_path = strjoin([subjfolder '/' S.name], '');
                 
             end
             
@@ -72,14 +83,15 @@ for subj = 1:length(MEGsubjlist_tab.BIDS_ID)
             
             subjfolder = strjoin([MEGsubjlist_tab.MRI_Dir(subj) '/' MEGsubjlist_tab.MRI_ID(subj)], '');
             
+            
             if isfolder(subjfolder)
                 
-                S = dir(subjfolder, '*.nii');
+                S = dir(fullfile(subjfolder, '*.nii'));
                 
                 S(startsWith({S.name},'y_')) = [];
                 
                 
-                MRI_path = fullfile(subjfolder, S.name);
+                MRI_path = strjoin([subjfolder '/' S.name], '');
                 
             end
             
@@ -89,7 +101,7 @@ for subj = 1:length(MEGsubjlist_tab.BIDS_ID)
             
             S = dir(fullfile(dir_path4,  strjoin([MEGsubjlist_tab.MRI_ID(subj) '*.nii'], '')));
             
-            MRI_path = fullfile(dir_path4, S.name);
+            MRI_path = [S.folder '/' S.name];
             
             
         elseif strcmp(MEGsubjlist_tab.MRI_Dir(subj), dir_path5)
@@ -99,36 +111,83 @@ for subj = 1:length(MEGsubjlist_tab.BIDS_ID)
             
             if isfolder(subjfolder)
                 
-                S = dir(subjfolder, '**/*.nii');
+                S = dir(fullfile(subjfolder, '**/*.nii'));
                 
                 
                 if contains(MEGsubjlist_tab.MRI_ID(subj), 'CBU')
                     
                     S(~startsWith({S.name}, 'sCBU')) = [];
                     
-                    MRI_path = fullfile(S.folder(1), S.name(1));
+                    MRI_path = fullfile(S(1).folder, S(1).name);
                     
                 else
                     
                     S(~startsWith({S.name}, MEGsubjlist_tab.MRI_ID(subj))) = [];
                     
-                    MRI_path = fullfile(S.folder(1), S.name(1));
+                    MRI_path = fullfile(S(1).folder, S(1).name);
                     
                     
                 end
                 
+                
             end
             
-            
-            
+                     
         end
         
         
+        %Push to output
+        
+        %Push to data table      
+        
+        if subj == 165
+            
+            MEGsubjlist_tab.found_MRI{subj} = [];
+            
+        else
+            
+            MEGsubjlist_tab.found_MRI{subj} = MRI_path;
+            
+            MEGsubjlist_tab.BIDS_fname_MRI{subj} = strjoin([MEGsubjlist_tab.BIDS_ID(subj) '' new_bids_fname_pfix], '');
+            
+            
+            %Raw Dir
+            
+            source_MRI = [MEGsubjlist_tab.found_MRI{subj}];
+            
+            target_MRI = [new_BIDSDIR '' MEGsubjlist_tab.BIDS_fname_MRI{subj}];
+            
+            try transfer_files(source_MRI, target_MRI); end
+            
+            
+            %Derivs Dir
+            
+            target_MRI = [new_BIDSDIR_deriv '' MEGsubjlist_tab.BIDS_fname_MRI{subj}];
+            
+            try transfer_files(source_MRI, target_MRI); end
+            
+        end
+        
     end
-    
     
 end
 
+
+    function transfer_files(source, target)
+        
+        
+        %Copy file to BIDS dir
+        
+        copyfile(source, target);
+        
+        
+        %Gzip and remove (as orig file is kept)
+        
+        gzip(target);
+        
+        delete(target);
+        
+    end
 
 
 end
