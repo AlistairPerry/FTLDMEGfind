3be0203a12623468e6f11bf7aa1fba3e72473761
AlistairPerry <alistairperry123@gmail.com>
2022-11-04 12:51:19 +0000

    start anon script

---
+++ Preproc/A_Scripts/T1_anonymize.m
@@ -0,0 +1,13 @@
+function T1_anonymize(BASE_BIDS_DIR)
+
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+subj_BIDSDIR_deriv
+
+s1_defaceMRI_onlytrim(subj_BIDSDIR_deriv)
+
+
+end
+
